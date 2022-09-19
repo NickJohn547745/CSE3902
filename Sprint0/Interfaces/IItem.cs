4ffe156dd06418560e3c6de572e70fa6d6e6a940
Nicholas Johnson <johnson.8777@buckeyemail.osu.edu>
2022-09-18 22:28:57 -0400

    Added an item interface

---
+++ Sprint0/Interfaces/IItem.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sprint0.Interfaces
+{
+    public interface IItem
+    {
+        void SetBaseDurability(int maxDurability);
+        int GetBaseDurability();
+
+        void SetBasePower(int basePower);
+        int GetBasePower();
+
+        void Update(GameTime gameTime);
+    }
+}
