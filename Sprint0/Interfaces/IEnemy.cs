fc12b2e1b523842a93a29db77cb6c4cd8dd05788
Nicholas Johnson <johnson.8777@buckeyemail.osu.edu>
2022-09-18 21:58:50 -0400

    Added an enemy interface

---
+++ Sprint0/Interfaces/IEnemy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public interface IEnemy
+{
+    /* Enemy health functions */
+    private class Health
+    {
+        void SetMaxHealth(int maxHealth);
+        int GetMaxHealth();
+
+        void SetHealth(int health);
+        int GetHealth();
+
+        void DecreaseHealth(int difference);
+        void IncreaseHealth(int difference);
+    }
+
+    /* Enemy item/weapon functions */
+    private class Item
+    {
+        void SetItem(object item);
+        //void SetItem(IItem item);
+        object GetItem();
+        //IItem GetItem();
+    }
+
+    void Update(GameTime gameTime);
+}
