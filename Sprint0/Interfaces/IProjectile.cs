dadd1b76d587ab46cc8d97bccd78c6e86442fbb6
Will Blanton <blanton.150@buckeyemail.osu.edu>
2022-09-20 21:18:16 -0400

    Added Projectile Interface.

---
+++ Sprint0/Interfaces/IProjectile.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace sprint0;
+
+public interface IProjectile
+{
+    public Rectangle GetPosition();
+
+    public void Update(GameTime gameTime);
+
+    public void Draw(SpriteBatch spriteBatch);
+}
