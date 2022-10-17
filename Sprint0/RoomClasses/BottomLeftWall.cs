e426fe5f575d39dca71b662e1239a9100151f248
Nicholas Johnson <johnson.8777@buckeyemail.osu.edu>
2022-10-17 10:30:08 -0400

    Added wall hitboxes & collisions

---
+++ Sprint0/RoomClasses/BottomLeftWall.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sprint0.RoomClasses
+{
+    public class BottomLeftWall : Wall
+    {
+        public BottomLeftWall()
+        {
+
+        }
+
+        public override Rectangle GetHitBox()
+        {
+            return new Rectangle(0, 589, 560, 135);
+        }
+    }
+}
