0fda9aeab2113928cafad50a366ee2f107518fc3
awilkes2020 <awilkes2020@yahoo.com>
2022-11-07 19:11:48 -0500

    Added HUD class and constructor

---
+++ Sprint0/Classes/HUD.cs
@@ -0,0 +1,25 @@
+﻿using sprint0.PlayerClasses;
+using sprint0.PlayerClasses.Abilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sprint0.Classes
+{
+    public class HUD
+    {
+        public Game1 Game { get; set; }
+        public PlayerInventory Inventory { get; set; }
+        AbilityTypes currentAbilityA { get; set; }
+        AbilityTypes currentAbilityB { get; set; }
+        public HUD (Game1 game, PlayerInventory inventory, AbilityTypes currentA, AbilityTypes currentB)
+        {
+            Game = game;
+            Inventory = inventory;
+            currentAbilityA = currentA;
+            currentAbilityB = currentB;
+        }
+    }
+}
